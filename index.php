2d46e4f3cd9e70bd1e6559866b5c1e1458b240e8
Jon Hinks <projects.ws@jondh.me.uk>
2018-04-30 22:39:36 +0100

    Add Phar index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This is the entry point for the Phar file
+ */
+
+require_once __DIR__ . '/src/DockerLs/Registry/GitLab.php';
+require_once __DIR__ . '/src/DockerLs/Registry/Exceptions/GeneralError.php';
+require_once __DIR__ . '/src/DockerLs/Registry/Exceptions/RegistryNotFound.php';
